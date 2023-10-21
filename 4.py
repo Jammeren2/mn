8ac57a3e38fc32b9ee4786c4c5ce89359b3881dd
Jammeren2 <52398081+Jammeren2@users.noreply.github.com>
2023-10-21 09:33:29 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,15 @@
+s = input("Введите последовательность чисел через пробел: ")
+ns = s.split()
+
+s_n = set()
+
+for n in ns:
+    if n in s_n:
+        print("YES")
+    else:
+        print("NO")
+        s_n.add(n)
+
+
+
+
